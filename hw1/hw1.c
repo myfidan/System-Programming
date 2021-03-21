71dd62b23dc7780de1adb6a638e00ccb8d480e5f
Yasir Fidan <yasirfidan@windowslive.com>
2021-03-21 23:26:57 +0300

    add getopt skeleton

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -1,9 +1,37 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
-int main(){
+int main(int argc, char *argv[]){
 
-	printf("Hello\n");
+	int opt;
 
+	while((opt = getopt(argc, argv, "fbtplw")) != -1)  
+    {  
+        switch(opt)  
+        {  
+            case 'f': 
+            	printf("option: %c\n", opt);
+                break; 
+            case 'b':  
+            	printf("option: %c\n", opt); 
+                break;
+            case 't':  
+                printf("option: %c\n", opt);
+                break;
+            case 'p':  
+                printf("option: %c\n", opt);
+                break;  
+            case 'l':  
+                printf("option: %c\n", opt);
+                break;
+			case 'w':  
+                printf("option: %c\n", opt);
+                break; 
+            default:
+            	printf("ERROR opt\n");  
+        }  
+    }
 
 	return 0;
 }
