0063d3b3d832fcc1f34f65b0d7d4456c7a34655a
Yasir Fidan <yasirfidan@windowslive.com>
2021-03-22 15:25:18 +0300

    add regular exp parser and some error condisitons for getotp

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
+
+//check a string is consist of a number or not
+//used for optarg in -b and -l paramaters
+int checkNumber(char* str){
+	while(*str != '\0'){
+		if(*str < '0' || *str > '9'){
+			return 0;
+		}
+		str++;
+	}
+	return 1;
+}
+
+//check a filename match with given regular expression filename or not
+//like lost+file match lostttttfile
+int checkRegularExp(char* filename, char* reg_file){
+	int reg_index=0,file_index=0;
+
+	while(reg_file[reg_index] != '\0'){
+
+		//normal character
+		if(reg_file[reg_index] != '+'){
+			if(reg_file[reg_index] != filename[file_index]){
+				return -1;//this 2 file not match
+			}
+		}//+ reg
+		else{
+			char prev_char = reg_file[reg_index-1];
+
+			while(filename[file_index] == prev_char){
+
+				file_index++;
+			}
+			file_index--;
+		}
+		reg_index++;
+		file_index++;
+	}
+
+	return 1;
+}
 
 int main(int argc, char *argv[]){
 
@@ -33,21 +75,50 @@ int main(int argc, char *argv[]){
                 break; 
             case 'b':  
             	flags_bool[1] = 1;
+
+            	int isNumberB = checkNumber(optarg);
+            	if(isNumberB == 0){
+            		printf("Error, b argument should be integer\n");
+            		return -1;
+            	}
             	file_size = atoi(optarg);
+            	
+        		
             	printf("option: %c\n", opt); 
                 break;
             case 't':
             	flags_bool[2] = 1; 
+            	//check t argument valid or not
+            	if(strlen(optarg) > 1 || (*optarg != 'd' && *optarg != 's' && 
+            		*optarg != 'b' && *optarg != 'c' && *optarg != 'f' &&
+            		*optarg != 'p' && *optarg != 'l')){
+
+            		printf("Error for argument -t\n");
+            		return -1;
+            	}
+
             	file_type = optarg[0];
                 printf("option: %c\n", opt);
                 break;
             case 'p':
-            	flags_bool[3] = 1; 
+            	flags_bool[3] = 1;
+            	//check -p valid or not
+            	if(strlen(optarg) != 9){
+            		printf("Error, -p argument must be 9 characters\n");
+            		return -1;
+            	}
             	permissions = optarg; 
                 printf("option: %c\n", opt);
                 break;  
             case 'l':
             	flags_bool[4] = 1; 
+
+            	int isNumberL = checkNumber(optarg);
+            	if(isNumberL == 0){
+            		printf("Error, b argument should be integer\n");
+            		return -1;
+            	}
+
             	link_number = atoi(optarg); 
                 printf("option: %c\n", opt);
                 break;
@@ -62,12 +133,12 @@ int main(int argc, char *argv[]){
         }  
     }
 
-    //Check there is w argument or not
+    //Check there is w argument or not because w argument mandatory
     if(flags_bool[5] == 0){
     	printf("ERROR, No w argument, its mandatory.\n");
     	return (-1);
     }
-    
+
     //Check there is at least 1 fbtpl argument
     int flag_count = 0;
     for (int i = 0; i < 5; ++i){
@@ -89,5 +160,7 @@ int main(int argc, char *argv[]){
     printf("%d\n", file_size);
     printf("%c\n", file_type);
 
+    int temp = checkRegularExp("losttttfile", "lost+fil+e+");
+    printf("%d\n", temp );
 	return 0;
 }
