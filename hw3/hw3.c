481cd979f2ec0b02038d416368a43b37eb784c45
Yasir Fidan <yasirfidan@windowslive.com>
2021-04-19 18:56:21 +0300

    print shared memory to potatoes id and number of switch

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -19,10 +19,12 @@
 #include <sys/mman.h>
 #define BUFF_SIZE 1024
 
+char* findFifo(char* addr, char* fifoFileBuffer);
+
 int main(int argc, char* argv[]){
 
 	int opt;
-	int hasporaroornot;
+	int haspotatoornot;
 	char* nameofsharedmemory;
 	char* filewithfifonames;
 	char* namedsemaphore;
@@ -32,7 +34,7 @@ int main(int argc, char* argv[]){
     {  
         switch(opt){
         	case 'b':
-        		hasporaroornot = atoi(optarg);
+        		haspotatoornot = atoi(optarg);
         		checkArg++;
         	break;
 
@@ -84,51 +86,104 @@ int main(int argc, char* argv[]){
     sema = sem_open(namedsemaphore,O_CREAT,0666,1);
 
     //Shared memory create
+    
+
+    //sem wait
+    sem_wait(sema);
+    //critical section //
+
+
     int fd_shm = shm_open(nameofsharedmemory, O_CREAT | O_RDWR,0666);
     if(fd_shm < 0){
     	perror("shm_open error");
     	return 1;
     }
     struct stat check_shm_stat;
+    char* addr;
 
     if(fstat(fd_shm,&check_shm_stat) == -1){
     	perror("fstat error");
     	return 1;
     }
 
-    if(check_shm_stat.st_size == 0){
+    char* x = "abc";
+    char delimeter = '-';
+    char* processFifoName;
+    if(check_shm_stat.st_size == 0){ //first time shared memory open
     	printf("shm size 0\n");
+    	ftruncate(fd_shm,BUFF_SIZE*4);
+		addr = mmap(NULL,BUFF_SIZE*4, PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm,0);
+		
+		processFifoName = findFifo(addr,fifoFileBuffer);
+		strncat(processFifoName,&delimeter,1);
+
+		printf("%s\n",processFifoName);
+		memcpy(addr,processFifoName,strlen(processFifoName)+1);
+
     } 
-    else{
+    else{	//not first time
     	printf("shm size not 0\n");
+    	addr = mmap(NULL,check_shm_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm,0);
+
+    	processFifoName = findFifo(addr,fifoFileBuffer);
+		strncat(processFifoName,&delimeter,1);
+
+		printf("%s\n",processFifoName);
+
+		strcat(addr,processFifoName);
+
+    	//write(STDOUT_FILENO,addr,check_shm_stat.st_size);
+    	//strcat(addr,x);
     }
 
-    char* addr = mmap(NULL,check_shm_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm,0);
+   	//Add potato id and number to shared memory
+    if(haspotatoornot > 0){
+    	char appendPotato[20];
+    	appendPotato[0] = '\0';
+    	snprintf (appendPotato, 20, "*%ld_%d*", (long)getpid(), haspotatoornot);
+    	strncat(appendPotato,&delimeter,1);
+    	strcat(addr,appendPotato);
+    }
+   
+    printf("%s\n",addr );
 
+    //critical section //
+    sem_post(sema);
+    //end critical section
     if(close(fd_shm) == -1){	// fd_shm is no longer needed  
     	perror("close error");
     	return 1;
     }
 
-    //sem wait
-    sem_wait(sema);
-    //critical section //
-
-    //critical section //
-    sem_post(sema);
-    //end critical section
-
-    printf("%d\n", hasporaroornot );
-    printf("%s\n",nameofsharedmemory );
-    printf("%s\n",filewithfifonames );
-    printf("%s\n",namedsemaphore );
-    printf("-------------------\n");
-    printf("%s\n",fifoFileBuffer );
+  
     //close and unlink shm
+    //close and unlink sema
     munmap(addr,check_shm_stat.st_size);
     shm_unlink(nameofsharedmemory);
-    //close and unlink sema
     sem_close(sema);
     sem_unlink(namedsemaphore);
 	return 0;
+}
+
+
+
+char* findFifo(char* addr, char* fifoFileBuffer){
+	char* findedFifoName;
+	char* ret;
+	const char s[2] = "\n";
+	char *token;
+	token = strtok(fifoFileBuffer, s);
+
+	while( token != NULL ){
+	    ret = strstr(addr,token);
+	    if(ret == NULL){
+	    	findedFifoName = token;
+	    	break;
+	    }
+
+	    token = strtok(NULL, s);
+    }
+
+    return findedFifoName;
+   
 }
