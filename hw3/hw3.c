d4d36d205b19089cbc8eb8a70df279e5e329df38
Yasir Fidan <yasirfidan@windowslive.com>
2021-04-21 18:43:21 +0300

    2 process can send potato once

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -22,6 +22,8 @@
 #define FIFO_PERM (S_IRUSR | S_IWUSR)
 
 char* findFifo(char* addr, char* fifoFileBuffer);
+char* find_write_fifo(char* addr);
+int take_potato_id(char* readFifo);
 
 int main(int argc, char* argv[]){
 
@@ -93,6 +95,8 @@ int main(int argc, char* argv[]){
     sem_t* sema;
     sema = sem_open(namedsemaphore,O_CREAT,0666,1);
 
+    sem_t* fifo_sema;
+    fifo_sema = sem_open("/fifosem",O_CREAT,0666,0);
     //Shared memory create
     
     //sem wait
@@ -113,7 +117,7 @@ int main(int argc, char* argv[]){
     	return 1;
     }
 
-    char* x = "abc";
+  
     char delimeter = '-';
     char* processFifoName;
     if(check_shm_stat.st_size == 0){ //first time shared memory open
@@ -142,6 +146,11 @@ int main(int argc, char* argv[]){
     }
 
    	//Add potato id and number to shared memory
+   	char current_potato_id[10];
+   	current_potato_id[0] = '\0';
+   	snprintf(current_potato_id,10,"%ld",(long)getpid());
+   	strncat(current_potato_id,&delimeter,1);
+
     if(haspotatoornot > 0){
     	char appendPotato[20];
     	appendPotato[0] = '\0';
@@ -162,23 +171,60 @@ int main(int argc, char* argv[]){
     sem_post(sema);
 
 
-    /*
+    
     int fifoFd;
-    if(haspotatoornot > 0){ // if process has potatp
-    	while(((fifoFd = open(processFifoName,O_WRONLY)) == -1) && (errno == EINTR));
+    if(haspotatoornot > 0){ // if process has potato
+    	
+    	sem_wait(fifo_sema);
+    	char* find_fifo_for_write = find_write_fifo(addr);
+    	printf("finded_fifo = %s\n",find_fifo_for_write );
+    	//delete last character R
+    	find_fifo_for_write[strlen(find_fifo_for_write)-1]= '\0';
+    	printf("finded_fifo_after_remove = %s\n",find_fifo_for_write );
+
+
+		
+    	while(((fifoFd = open(find_fifo_for_write,O_WRONLY)) == -1) && (errno == EINTR));
     	if(fifoFd == -1){
     		perror("open fifo error");
     		return 1;
     	}
+    	printf("%ld sending potato number %d to %s\n",(long)getpid(),take_potato_id(current_potato_id) ,find_fifo_for_write);
+    	write(fifoFd,current_potato_id,strlen(current_potato_id));
+
     }
     else{
+    	//add his fifo to shared memory to find a write process
+
+    	sem_wait(sema); // block shared memory because I will use it
+    	// /home/cse312/Desktop/fifos/fifo2-/home/cse312/Desktop/fifos/fifo2R-
+    	char fifo_read_name[200];
+    	fifo_read_name[0] = '\0';
+    	char readDelimeter = 'R';
+    	strcat(fifo_read_name,processFifoName);
+    	strncat(fifo_read_name,&readDelimeter,1);
+    	strncat(fifo_read_name,&delimeter,1);
+    	strcat(addr,fifo_read_name);
+
+    	sem_post(sema); // unblock shared memory my work is done with it
+
+    	sem_post(fifo_sema);
+    	
     	while(((fifoFd = open(processFifoName,O_RDONLY)) == -1) && (errno == EINTR));
     	if(fifoFd == -1){
     		perror("open fifo error");
     		return 1;
     	}
+    	char readFifo[20];
+    	readFifo[0] = '\0';
+    	char car;
+    	do{
+    		read(fifoFd,&car,1);
+    		strncat(readFifo,&car,1);
+    	}while(car != '-');
+    	printf("%ld receiving potato number %d from %s\n", (long)getpid(),take_potato_id(readFifo),processFifoName);
     }
-*/
+
     //end critical section
     if(close(fd_shm) == -1){	// fd_shm is no longer needed  
     	perror("close error");
@@ -188,10 +234,13 @@ int main(int argc, char* argv[]){
   
     //close and unlink shm
     //close and unlink sema
-    munmap(addr,check_shm_stat.st_size);
-    shm_unlink(nameofsharedmemory);
-    sem_close(sema);
-    sem_unlink(namedsemaphore);
+    //munmap(addr,check_shm_stat.st_size);
+    //shm_unlink(nameofsharedmemory);
+    //sem_close(sema);
+    //sem_unlink(namedsemaphore);
+
+    //sem_close(fifo_sema);
+    //sem_unlink("/fifosem");
 	return 0;
 }
 
@@ -216,4 +265,30 @@ char* findFifo(char* addr, char* fifoFileBuffer){
 
     return findedFifoName;
    
+}
+
+char* find_write_fifo(char* addr){
+	char* tempstr = calloc(strlen(addr)+1, sizeof(char));
+	strcpy(tempstr, addr);
+
+	const char s[2] = "-";
+	char *token;
+	token = strtok(tempstr, s);
+
+
+	while( token != NULL ){
+	    if(token[strlen(token)-1] == 'R'){
+	    	free(tempstr);
+	    	return token;
+	    }
+
+	    token = strtok(NULL, s);
+    }
+
+	free(tempstr);
+	return NULL;
+}
+
+int take_potato_id(char* readFifo){
+	return atoi(readFifo);
 }
